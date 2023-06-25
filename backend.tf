61d2b079b7963646026c9c179c1bd051bcbb1102
SanathKumar619 <sanathkumarsanath3@gmail.com>
2023-06-25 10:09:21 +0000

    backend-lock

--- backend.tf
+++ backend.tf
@@ -1,8 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "sanath-terraform-backend"
-    key    = "EC2/terraform.tfstate"
-    region = "us-east-1"
+    bucket         = "sanath-terraform-backend"
+    key            = "EC2/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "EC2-state-lock"
   }
 }
 
